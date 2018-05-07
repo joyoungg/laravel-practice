d534f3377c53eb13e5b5c066ba2d46faddc938ee
joyoungg <whduddl9212>
2018-05-07 23:55:42 +0900

    할일 완료상태 이동

--- resources/assets/js/default/todo.js
+++ resources/assets/js/default/todo.js
@@ -1,113 +1,126 @@
 new Vue({
-  el: '#app',
-  data: function () {
-    return {
-      todoData: {},
-      todoList: {},
-      todo: {},
-    }
-  },
-  mounted: function () {
-    this.$nextTick(function () {
-      this.getList()
-      this.getContent()
-      console.log('mounted!')
-    })
-  },
-  methods: {
-    getContent: function () {
-      axios.get('/api/getTodoContent').then(response => {
-        this.todo = response.data.data
-      })
-    },
-    getList: function () {
-      axios.get('/api/getTodoTitle').then(response => {
-        this.todoList = response.data.data
-      })
-    },
-    addTitle: function () {
-      axios.post('/api/addTodo', this.todoData).then(result => {
-        if (result.status == 200) {
-          console.log('등록!')
-          this.getList()
+    el: '#app',
+    data: function () {
+        return {
+            todoData: {},
+            todoList: {},
+            todo: {},
         }
-      }, errors => {
-        console.log(errors)
-      })
     },
-    addTodo: function (id) {
-      this.todoData.title_id = id
-      axios.post('/api/addTodo', this.todoData).then(result => {
-        if (result.status == 200) {
-          console.log('등록!')
-          this.getContent()
-        }
-      }, errors => {
-        console.log(errors)
-      })
+    mounted: function () {
+        this.$nextTick(function () {
+            this.getList()
+            this.getContent()
+        })
     },
-    change: function (id) {
-      this.todoData.id = id
-      axios.post('/api/done', this.todoData).then(result => {
-        console.log(result)
-        this.getContent()
-      }, errors => {
-        console.log(errors)
-      })
-    },
-    deleteItem: function (id) {
-      this.todoData.id = id
-      axios.delete('/api/erase/item/' + this.todoData.id)
-      this.getContent()
-    },
-    deleteTitle: function (id) {
-      this.todoData.id = id
-      axios.delete('/api/erase/title/' + this.todoData.id)
-      this.getList()
-    },
-    editTitle: function (id, title) {
-      this.todoData.id = id
-      this.todoData.title = title
-      axios.patch('/api/edit/title', this.todoData).then(result => {
-      }, errors => {
-        console.log(errors)
-      })
-    },
-    drag: function () {
-      console.log('drag')
-    },
-    dragstart: function (event) {
-      event.target.style.opacity = .5
-    },
-    dragover: function (event) {
-      //prevent default to allow drop
-      event.preventDefault()
-      console.log('drag over')
-    },
-    drop: function (event) {
-      event.preventDefault()
-      console.log(123)
-      // move dragged elem to the selected drop target
-      if (event.target.className == 'dropzone') {
-        event.target.style.background = ''
-        dragged.parentNode.removeChild(dragged)
-        event.target.appendChild(dragged)
-      }
-      var data = event.dataTransfer.getData('Text')
-      console.log(data)
+    methods: {
+        getContent: function () {
+            axios.get('/api/getTodoContent').then(response => {
+                this.todo = response.data.data
+            })
+        },
+        getList: function () {
+            axios.get('/api/getTodoTitle').then(response => {
+                this.todoList = response.data.data
+            })
+        },
+        addTitle: function () {
+            axios.post('/api/addTodo', this.todoData).then(result => {
+                if (result.status == 200) {
+                    console.log('등록!')
+                    this.getList()
+                }
+            }, errors => {
+                console.log(errors)
+            })
+        },
+        addTodo: function (id) {
+            this.todoData.title_id = id
+            axios.post('/api/addTodo', this.todoData).then(result => {
+                if (result.status == 200) {
+                    console.log('등록!')
+                    this.getContent()
+                }
+            }, errors => {
+                console.log(errors)
+            })
+        },
+        change: function (id) {
+            this.todoData.id = id
+            axios.post('/api/done', this.todoData).then(result => {
+                console.log(result)
+                this.getContent()
+            }, errors => {
+                console.log(errors)
+            })
+        },
+        deleteItem: function (id) {
+            this.todoData.id = id
+            axios.delete('/api/erase/item/' + this.todoData.id)
+            this.getContent()
+        },
+        deleteTitle: function (id) {
+            this.todoData.id = id
+            axios.delete('/api/erase/title/' + this.todoData.id)
+            this.getList()
+        },
+        editTitle: function (id, title) {
+            this.todoData.id = id
+            this.todoData.title = title
+            axios.patch('/api/edit/title', this.todoData).then(result => {
+            }, errors => {
+                console.log(errors)
+            })
+        },
+        dragstart: function (event) {
+            event.target.style.opacity = '0.5'
+            event.dataTransfer.effectAllowed = 'move'
+            event.dataTransfer.setData("text/plain", event.target.id);
+        },
+        dragover: function (event) {
+            event.preventDefault()
+        },
+        drop: function (event) {
+            event.preventDefault()
+            let data = event.dataTransfer.getData("text/plain")
+            let dragged = document.getElementById(data)
+            console.log(event.target.parentNode.parentNode)
+            if (!data) {
+                console.log('nothing!!')
+            }
+            else {
+                console.log('data part')
+                if (event.target.parentNode.parentNode.className == 'completezone' || 'todozone') {
+                    event.target.style.background = ''
+                    dragged.parentNode.removeChild(dragged)
+                    event.target.parentNode.parentNode.insertAdjacentElement('afterend', dragged);
+                }
+            }
+            if (dragged.parentNode.className != event.target.parentNode.id) {
+                this.change(dragged.id)
+            }
 
-    },
-    dragend: function (event) {
-      // reset the transparency
-      event.target.style.opacity = ''
-      console.log('eeeeeeeeeee')
-    },
-    dragenter: function () {
-      // highlight potential drop target when the draggable element enters it
-      if (event.target.className == 'dropzone') {
-        event.target.style.background = 'purple'
-      }
-      console.log('drag enter')
+        },
+        dragenter: function (event) {
+            event.preventDefault()
+            // highlight potential drop target when the draggable element enters it
+            if (event.target.parentNode.parentNode.className == 'completezone' || 'todozone') {
+                event.target.style.background = 'purple'
+            }
+        },
+        dragleave: function (event) {
+            event.preventDefault()
+            if (event.target.parentNode.parentNode.className == 'completezone' || 'todozone') {
+                event.target.style.background = "";
+            }
+        },
+        allowDrop: function (event) {
+            if (event.target.getAttribute('draggable') == 'true') {
+                event.preventDefault()
+            }
+            else {
+                event.dataTransfer.dropEffect = 'all'
+            }
+        }
     }
-  }
 })
--- resources/views/default/todo.blade.php
+++ resources/views/default/todo.blade.php
@@ -1,124 +1,98 @@
 @extends('layout.default')
 
 @section('content')
-  <div data-controller="todo">
+	<div data-controller="todo">
 
-    <div class="input-group mb-3">
-      <input type="text" class="form-control" placeholder="할 일을 입력해주세요" aria-label="todo" v-model="todoData.title">
-      <div class="input-group-append">
-        <button class="btn btn-outline-secondary" @click="addTitle">추가</button>
-      </div>
-    </div>
+		<div class="input-group mb-3">
+			<input type="text" class="form-control" placeholder="할 일을 입력해주세요" aria-label="todo"
+						 v-model="todoData.title">
+			<div class="input-group-append">
+				<button class="btn btn-outline-secondary" @click="addTitle">추가</button>
+			</div>
+		</div>
 
-    <table class="table">
-      <thead>
-      <tr>
-        <th>title</th>
-        <th>todo</th>
-        <th>done</th>
-      </tr>
-      </thead>
-      <tbody>
-      <tr v-for="(item, index) in todoList">
-        <td>
-          {{--<span>@{{ item.title }}</span>--}}
-          <input type="text" v-model="item.title" style="border: 0px" @keyup.enter="editTitle(item.id, item.title)">
-          {{--버튼--}}
-          <div class="btn-group pull-right"
-               style="font-size: 12px; line-height: 1;">
-            <button type="button"
-                    class="btn-link dropdown-toggle"
-                    data-toggle="dropdown"
-                    aria-haspopup="true"
-                    aria-expanded="false">add<span class="caret"></span>
-            </button>
-            <ul class="dropdown-menu">
-              <li>
-                <div>
-                  <form @submit.prevent="addTodo(item.id)">
-                    <div class="form-group">
-                      <label for="content" class="col-form-label">내용</label>
-                      <span>@{{ item.id }}</span>
-                      <input class="form-control" id="content" v-model="todoData.content">
-                      <button> 등록</button>
-                    </div>
-                  </form>
-                </div>
-              </li>
-              <li>
-                <button type="button" style="border:none; font-size:5px" @click="deleteTitle(item.id)">삭제</button>
-              </li>
-            </ul>
-          </div>
-        </td>
+		<table class="table">
+			<thead>
+			<tr>
+				<th>title</th>
+				<th>todo</th>
+				<th>done</th>
+			</tr>
+			</thead>
+			<tbody>
+			<tr v-for="(item, index) in todoList">
+				<td>
+					{{--<span>@{{ item.title }}</span>--}}
+					<input type="text" v-model="item.title" style="border: 0px"
+								 @keyup.enter="editTitle(item.id, item.title)">
+					{{--버튼--}}
+					<div class="btn-group pull-right"
+							 style="font-size: 12px; line-height: 1;">
+						<button type="button"
+										class="btn-link dropdown-toggle"
+										data-toggle="dropdown"
+										aria-haspopup="true"
+										aria-expanded="false">add<span class="caret"></span>
+						</button>
+						<ul class="dropdown-menu">
+							<li>
+								<div>
+									<form @submit.prevent="addTodo(item.id)">
+										<div class="form-group">
+											<label for="content" class="col-form-label">내용</label>
+											<span>@{{ item.id }}</span>
+											<input class="form-control" id="content" v-model="todoData.content">
+											<button> 등록</button>
+										</div>
+									</form>
+								</div>
+							</li>
+							<li>
+								<button type="button" style="border:none; font-size:5px" @click="deleteTitle(item.id)">
+									삭제
+								</button>
+							</li>
+						</ul>
+					</div>
+				</td>
 
-        <td>
-          {{--투두 목록--}}
-          <div v-for="(data, index) in todo" v-if="(item.id == data.title_id&&data.status == 0)">
-            {{--<span dragover="allowDrop(event)" drop="drop(event)">--}}
-            {{--<input type="text" v-model="data.content" style="border: 0px"--}}
-            {{--@keyup.enter="editText(data.id, data.content)">--}}
-            {{--</span>--}}
+				<td class="todozone" v-on:dragover="allowDrop($event)">
+					{{--투두 목록--}}
+					<div v-for="(data, index) in todo" v-if="(item.id == data.title_id&&data.status == 0)" draggable="true"							 v-bind:id="data.id"
+							 v-on:dragstart="dragstart($event)"
+							 v-on:dragover="dragover($event)"
+							 v-on:dragleave="dragleave($event)"
+							 v-on:dragenter="dragenter($event)"
+							 v-on:drop="drop($event)">
+						<div id="todozone">
+							<input type="text" v-model="data.content" style="border: 0px"
+										 @keyup.enter="editText(data.id, data.content)">
+							{{--@{{ data.content }}--}}
+							<button type="button" style="border:none; font-size:5px" @click="change(data.id)">완료</button>
+						</div>
+					</div>
+				</td>
 
-            <span class="dropzone" draggable="true"
-                  v-on:drag="drag($event)" v-on:dragstart="dragstart($event)" v-on:dragover="dragover($event)" v-on:drop="drop($event)"
-                  v-on:dragend="dragend($event)" v-on:dragenter="dragenter($event)">
-              @{{ data.content }}
-            </span>
-            <span>
-              <button type="button" style="border:none; font-size:5px" @click="change(data.id)">완료</button>
-            </span>
+				{{--완료 목록--}}
+				<td class="completezone" v-on:dragover="allowDrop($event)">
+					<div v-for="(data, index) in todo" v-if="(item.id == data.title_id&&data.status == 1)" draggable="true"
+							 v-bind:id="data.id"
+							 v-on:dragstart="dragstart($event)"
+							 v-on:dragover="dragover($event)"
+							 v-on:dragleave="dragleave($event)"
+							 v-on:dragenter="dragenter($event)"
+							 v-on:drop="drop($event)">
+						<div id="completezone" >
+							@{{ data.content }}
+							<button type="button" style="border:none; font-size:5px" @click="deleteItem(data.id)">삭제</button>
+						</div>
+					</div>
+				</td>
 
-          </div>
-        </td>
-
-        {{--완료 목록--}}
-        <td>
-          <div v-for="(data, index) in todo" v-if="(item.id == data.title_id&&data.status == 1)">
-            <span class="dropzone">
-                @{{ data.content }}
-            </span>
-            <span>
-              <button type="button" style="border:none; font-size:5px" @click="deleteItem(data.id)">삭제</button>
-            </span>
-          </div>
-        </td>
-
-      </tr>
-      </tbody>
-    </table>
-
-    {{--<div v-for="(item, index) in todoList">--}}
-    {{--<div>--}}
-    {{--@{{ item.title }}--}}
-    {{--<div class="btn-group pull-right"--}}
-    {{--style="font-size: 12px; line-height: 1;">--}}
-    {{--<button type="button"--}}
-    {{--class="btn-link dropdown-toggle"--}}
-    {{--data-toggle="dropdown"--}}
-    {{--aria-haspopup="true"--}}
-    {{--aria-expanded="false">add<span class="caret"></span>--}}
-    {{--</button>--}}
-    {{--<ul class="dropdown-menu">--}}
-    {{--<li>--}}
-    {{--<div>--}}
-    {{--<form @submit="addTodo(item.id)">--}}
-    {{--<div class="form-group">--}}
-    {{--<input type="hidden" value="item.title" v-model="todoData.title">--}}
-    {{--<label for="content" class="col-form-label">내용</label>--}}
-    {{--<input class="form-control" id="content" v-model="todoData.content">--}}
-    {{--<button> 등록 </button>--}}
-    {{--</div>--}}
-    {{--</form>--}}
-    {{--</div>--}}
-    {{--</li>--}}
-    {{--</ul>--}}
-    {{--</div>--}}
-    {{--<span class="text-right">--}}
-    {{--<button type="button" style="border:none; font-size:5px" @click="change(item.id)">완료</button>--}}
-    {{--<button type="button" style="border:none; font-size:5px" @click="erase(item.id)">삭제</button>--}}
-    {{--</span>--}}
-  </div>
+			</tr>
+			</tbody>
+		</table>
+	</div>
 
 
 
